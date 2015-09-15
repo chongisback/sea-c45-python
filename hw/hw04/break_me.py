f7413e36551f82c75906d4e3c6f17cc08bf58d20
chong <chongisback@hotmail.com>
2015-09-14 22:21:06 -0700

    added new line at the end of the file

--- hw/hw04/break_me.py
+++ hw/hw04/break_me.py
@@ -1,15 +1,15 @@
 import math
 def exhibit_name_error():
-	# Replace this line with something that exhibits
-	# a name error
-	print(sehfilmflsakdnfldf)
+    # Replace this line with something that exhibits
+    # a name error
+    print(sehfilmflsakdnfldf)
 
 def exhibit_attribute_error():
-	# Replace this line with something that exhibits
-	# an attribute error
-	print(math.shlfkdflkmekekf)
+    # Replace this line with something that exhibits
+    # an attribute error
+    print(math.shlfkdflkmekekf)
 
 def exhibit_type_error():
-	# Replace this line with something that exhibits
-	# an type error
-	print(1+"53i4jfmsadgdsaf")
+    # Replace this line with something that exhibits
+    # an type error
+    print(1 + "53i4jfmsadgdsaf")
--- hw/hw04/test_break_me.py
+++ hw/hw04/test_break_me.py
@@ -9,7 +9,7 @@ code that tests the break_me.py functions
 
 import break_me
 
-
+# bad variable name
 def test_name_error():
     try:
         break_me.exhibit_name_error()
@@ -17,7 +17,7 @@ def test_name_error():
     except NameError:
         assert(True)
 
-
+# member of variable ex. math.kevin
 def test_attribute_error():
     assert('exhibit_attribute_error' in dir(break_me))
     try:
@@ -26,7 +26,7 @@ def test_attribute_error():
     except AttributeError:
         assert(True)
 
-
+# type error adding two variable of two types
 def test_type_error():
     try:
         break_me.exhibit_type_error()
