970b08de8cfefe009c1e46da6971f279c6378fd7
chong <chongisback@hotmail.com>
2015-09-16 19:41:49 -0700

    added extra line to fix linter error

--- hw/hw04/break_me.py
+++ hw/hw04/break_me.py
@@ -1,13 +1,17 @@
 import math
+
+
 def exhibit_name_error():
     # Replace this line with something that exhibits
     # a name error
     print(sehfilmflsakdnfldf)
 
+
 def exhibit_attribute_error():
     # Replace this line with something that exhibits
     # an attribute error
     print(math.shlfkdflkmekekf)
+
 
 def exhibit_type_error():
     # Replace this line with something that exhibits
