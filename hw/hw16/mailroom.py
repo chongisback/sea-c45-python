53d71ffe8374e8e20c3b27a479fdec80019b00f5
chong <chongisback@hotmail.com>
2015-09-29 01:24:00 -0700

    changed list to dictionary for storing data type. Re-did sorting algorithm. Re-did table

--- hw/hw16/mailroom.py
+++ hw/hw16/mailroom.py
@@ -1,16 +1,39 @@
 import os
 donors = []
+name_d = {}
+
+MAIN_MENU = u"\n\
+Choose from the following: \n\
+T - Send a (T)hank you \n\
+R - Create a (R)eport \n\
+quit - Quit the program \n\
+> "
+
+THANKS_MENU = u"\n\
+Please enter a name or choose from the following: \n\
+list - Print a list of previous donors\n\
+quit - Return to main menu \n\
+> "
+
+LETTER = u"\n\
+Dear %s, \n\
+\n\
+    Thank you so much for your kind donation of $%.2f. We here at the\n\
+Foundation for Everyone Needs Potato Salad greatly appreciate it. You \n\
+money will go towards researching the best way for everyone in the world\n\
+to enjoy potato salad.\n\
+\n\
+Thanks again,\n\
+\n\
+Chong Park\n\
+\n\
+Director, Foundation for Everyone Needs Potato Salad\n\n\n"
 
 
 def menu():
     global donors
     os.system('clear')
-    choice = input("\n\
-Choose from the following: \n\
-T - Send a (T)hank you \n\
-R - Create a (R)eport \n\
-quit - Quit the program \n\
-> ")
+    choice = input(MAIN_MENU)
     if(choice == 'T' or choice == 't'):
         thank_you_name()
     elif(choice == 'R' or choice == 'r'):
@@ -28,11 +51,7 @@ def thank_you_name():
     global donors
     while True:
         os.system('clear')
-        name = input(u"\n\
-Please enter a name or choose from the following: \n\
-list - Print a list of previous donors\n\
-quit - Return to main menu \n\
-> ")
+        name = input(THANKS_MENU)
         if(name == 'quit'):
             return
         # If name is 'list' loop does not break nor return
@@ -43,29 +62,23 @@ quit - Return to main menu \n\
             print("-------------")
             sorted_name = sort_by_name()
             for donor in sorted_name:
-                print(donor[0])
+                print(donor)
             input(u"\nPress enter to continue...")
         elif(name.replace(' ', '').isalpha()):
             break
     # If the loop breaks, name is neither 'quit' or 'list'
     name = name.title()
-    # Uses name to check for contain boolean and uses it with index
-    contains_and_index = contains_index(name)
-    contains, index = contains_and_index[0], contains_and_index[1]
-    if(not contains):
-        donors.append([name, 0, 0])
-    # Add donation
-    donation = add_donation(index)
+    # Dictionary version
+    if(name not in name_d.keys()):
+        name_d[name] = [0, 0]
+    donation = add_donation_d(name)
     if(donation == 'quit'):
         return
     # Print a letter
     create_a_letter(name, donation)
 
 
-def add_donation(index):
-    """
-    This function adds donation to the donor list
-    """
+def add_donation_d(name):
     os.system('clear')
     global donors
     while True:
@@ -75,8 +88,8 @@ def add_donation(index):
         elif(is_number(amount)):
             # amount is rounded to 2 decimal place
             amount = float("{0:.2f}".format(float(amount)))
-            donors[index][1] += amount
-            donors[index][2] += 1
+            name_d[name][0] += amount
+            name_d[name][1] += 1
             return amount
 
 
@@ -85,19 +98,7 @@ def create_a_letter(name, donation):
     This function prints a "Thank you" letter
     """
     os.system('clear')
-    print(u"\n\
-Dear %s, \n\
-\n\
-    Thank you so much for your kind donation of $%.2f. We here at the\n\
-Foundation for Everyone Needs Potato Salad greatly appreciate it. You \n\
-money will go towards researching the best way for everyone in the world\n\
-to enjoy potato salad.\n\
-\n\
-Thanks again,\n\
-\n\
-Chong Park\n\
-\n\
-Director, Foundation for Everyone Needs Potato Salad\n\n\n" % (name, donation))
+    print(LETTER % (name, donation))
     # Exception check for quitting after printing the letter
     # This was added to pass the test_mailroom.py
     exception = input("Press Enter to continue....")
@@ -123,50 +124,30 @@ def contains_index(name):
 
 
 def create_report():
-    """
-    Executed when R is pressed in the main menu.
-    It creates a report in table format.
-    Items on the columns are joined by tabs to create a tabular
-    table. The tab-ing is determined by the length of variable in the
-    column.
-    Format follows:
-    Name            |Total           |#       |Average Donation
-    -----------------------------------------------------------
-    1234567890123456|1234567890123456|12345678|12345....
-    Example Name    |1000.00         |2       |500.00
-    where number is how many spaces are in each column
-    """
     os.system('clear')
-    global donors
-    # Using variable tab and tab2 as tabs for spacing
-    tab = "\t|"
-    tab2 = "\t\t|"
-    s_l = sort_by_donation()
+    global name_d
+    sp = column_length()
+    s_n = sort_by_donation()
+    print (s_n)
     # newlist will hold the string values for each row of donor information
     newlist = []
-    for x in s_l:
+    for x in s_n:
         # avg is calculated by total donation / # of donation
         # it will get float with 2 decimal values
-        avg = '%.2f' % (float(x[1]) / x[2])
+        avg = '%.2f' % (float(name_d[x][0]) / name_d[x][1])
         # row holds list of each items of donor info.
         # row = [name, total, # donation, avg]
-        row = [x[0], '%.2f' % x[1], str(x[2]), str(avg)]
+        row = [x, '$%.2f' % name_d[x][0], str(name_d[x][1]), '$%s' % str(avg)]
         # Below is tabbing for correct tab space depends on length of the name
         # or donation amount. Max char per name and total donation
         # is 16. It will only account for that much tabbing at the moment
-        if(len(row[0]) > 7):
-            r_i = [(tab).join(row[:2]), (tab).join(row[2:])]
-        else:
-            r_i = [(tab2).join(row[:2]), (tab).join(row[2:])]
-        if(len(row[1]) >= 7):
-            row_items = (tab).join(r_i)
-        else:
-            row_items = (tab2).join(r_i)
+        row_i = [sp[s].format(x) for (s, x) in enumerate(row)]
+        row_items = ' | '.join(row_i)
         newlist.append(row_items)
 
     col = ["Name", "Total", "#", "Average Donation"]
-    subtopic = [(tab2).join(col[:2]), (tab).join(col[2:])]
-    columns = (tab2).join(subtopic)
+    c = [sp[s].format(x) for (s, x) in enumerate(col)]
+    columns = ' | '.join(c)
     print(columns)
     print("-----------------------------------------------------------")
     for items in newlist:
@@ -179,39 +160,69 @@ def create_report():
         quit()
 
 
+def column_length():
+    """
+    Calculates how many spaces the columns need based on
+    max number of characters for each characters. It will resize
+    the column to fit any data.
+    """
+    global name_d
+    a, b, c = 0, 0, 0
+    for name in name_d.keys():
+        if(len(name) > a):
+            a = len(name)
+        if(len(str(name_d[name][0])) > b):
+            b = len(str(name_d[name][0]))
+        if(len(str(name_d[name][1])) > c):
+            c = len(str(name_d[name][1]))
+    # default values for the table column
+    if(a < 4):
+        a = 4
+    if(b < 5):
+        b = 5
+    if(c < 1):
+        c = 1
+    # Ran past the 80 character limit. Added lists together
+    spaces = ['{:<%s}' % a, '{:>%s}' % (b + 2)]
+    spaces += ['{:>%s}' % c, '{:^%s}' % (b - c + 1)]
+    return spaces
+
+
 def sort_by_donation():
     """
-    Sorts donors list to a new list in order of most to least donation
-    This sorting algorithm uses insertion method.
+    Returns a list of name based on total donation amount.
     """
-    global donors
+    global name_d
     # s_l is short hand for sorted_list
     # d is short hand for single donor in donors
     # s_i is short hand for sorted index
     s_l = []
-    for d in donors:
+    s_d = []
+    for k in name_d.keys():
         # d is short hand for sorted_list
-        if(len(s_l) == 0):
+        if(len(s_d) == 0):
             sorted_i = 0
         else:
             # current d's total donation is greater than the 1st value in
             # sorted_list's total donation
-            if(d[1] >= s_l[0][1]):
+            if(name_d[k][0] >= s_d[0]):
                 sorted_i = 0
             # current d's total donation is less than the last value in
             # sorted_list's total donation
-            elif(d[1] < s_l[-1][1]):
-                sorted_i = len(s_l)
+            elif(name_d[k][0] < s_d[0]):
+                sorted_i = len(s_d)
             # otherwise, look for index where d's total donation can squeen
             # in between
             else:
                 i = 0
-                while (i < len(s_l) - 1):
-                    if(s_l[i][1] <= d[1] and s_l[i + 1][1] > d[1]):
+                while (i < len(s_d) - 1):
+                    if(s_d[i] <= name_d[k][0] and
+                            s_d[i + 1][0] > name_d[k][0]):
                         sorted_i = i + 1
                         break
                     i += 1
-        s_l.insert(sorted_i, d)
+        s_d.insert(sorted_i, name_d[k][0])
+        s_l.insert(sorted_i, k)
     return s_l
 
 
@@ -223,30 +234,30 @@ def sort_by_name():
     # s_l is short hand for sorted_list
     # d is short hand for single donor in donors
     # s_i is short hand for sorted index
-    global donors
+    global name_d
     s_l = []
-    for d in donors:
+    for key in name_d.keys():
         if(len(s_l) == 0):
             sorted_i = 0
         else:
             # current d's name is less than the 1st value in
             # sorted_list's name
-            if(d[0] < s_l[0][0]):
+            if(key < s_l[0]):
                 sorted_i = 0
             # current d's name is bigger than the last value in
             # sorted_list's name
-            elif(d[0] > s_l[-1][0]):
+            elif(key > s_l[-1]):
                 sorted_i = len(s_l)
             # otherwise, look for index where d's name can squeeze
             # in between
             else:
                 i = 0
                 while(i < len(s_l) - 1):
-                    if(s_l[i][0] < d[0] and s_l[i + 1][0] > d[0]):
+                    if(s_l[i] < key and s_l[i + 1] > key):
                         sorted_i = i + 1
                         break
                     i += 1
-        s_l.insert(sorted_i, d)
+        s_l.insert(sorted_i, key)
     return s_l
 
 
