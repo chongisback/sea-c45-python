5e622006bd0954795ff3a8121fefa12db943617b
chong <chongisback@hotmail.com>
2015-09-30 22:56:19 -0700

    added few lines of codes from class

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -8,4 +8,17 @@ Python class example.
 # The start of it all:
 # Fill it all in here.
 class Element(object):
+
+    def __init__(self, name=""):
+        self.name = name
+        self.children = []
+        pass
+
+    def render(self, f):zz
+        f.write("<{name}>".format(name=self.name))
+        # TODO: Write out the children here
+        f.write("</{name}".format(name=self.name))
+
+    def append(self, child):
+        self.children.append(child)
     pass
