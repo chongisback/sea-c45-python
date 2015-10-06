a78713407c9dee81b857a948753b91db9b20a202
chong <chongisback@hotmail.com>
2015-10-06 16:56:28 -0700

    still working on it.

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -10,4 +10,17 @@ class Circle(object):
     def __init__(self, r):
         self.radius = r
         self.diameter = r * 2
-        self.area = math.pi * r * r
+        self.area = math.pi * r ** 2
+
+    def _get_diameter(self):
+        return self._diameter
+
+    def _set_diameter(self, value):
+        self._diameter = value
+        self.raidus = value / 2
+        self.area = math.pi * self.radius ** 2
+
+    def _del_diameter(self):
+        del self._diameter
+
+    diameter = property(_get_diameter, _set_diameter, _del_diameter, "diameter")
