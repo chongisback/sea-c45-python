d34ff9afc379d8d976fd7d2414dc9527c2eb8f54
chong <chongisback@hotmail.com>
2015-10-06 21:14:53 -0700

    re-did work with correct properties. and added some mathematical operation capable functions.

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -5,19 +5,52 @@ fill this in so it will pass all the tests.
 """
 import math
 
+
 class Circle(object):
     def __init__(self, r):
+        self._diameter = r * 2
         self.radius = r
-        self.diameter = r * 2
-        self.area = math.pi * r ** 2
+        self._area = math.pi * r ** 2
 
-    def _get_diameter(self):
+    def __str__(self):
+        return 'Circle with radius: {r}'.format(r="%.6f" % self.radius)
+
+    def __repr__(self):
+        return 'Circle({r})'.format(r=self.radius)
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other)
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __gt__(self, other):
+        return self.radius > other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __ge__(self, other):
+        return self.radius >= other.radius
+
+    def __le__(self, other):
+        return self.radius <= other.radius
+
+    def _getd(self):
         return self._diameter
 
-    def _set_diameter(self, value):
+    def _setd(self, value):
         self._diameter = value
-        self._raidus = value / 2
+        self.radius = value / 2
 
-    def _del_diameter(self):
+    def _deld(self):
         del self._diameter
-    x = property(_get_diameter, _set_diameter, _del_diameter, doc="docstring")
+    diameter = property(_getd, _setd, _deld, doc="docstring")
+
+    def _geta(self):
+        return self._area
+
+    area = property(_geta, doc="docstring")
